c1c17256ff82205eae0755107d1f84a2b7dc6e1c
Symi <symi.contact@gmail.com>
2020-10-12 16:02:57 +0200

    Main page & category activity

--- app/src/main/java/com/example/healthysanity/MainActivity.java
+++ app/src/main/java/com/example/healthysanity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.healthysanity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout categoryLayout = findViewById(R.id.layoutCategory);
+        categoryLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCategoryActivity();
+            }
+        });
+
     }
+
+    private void openCategoryActivity()
+    {
+        Intent intent = new Intent(this, CategoryList.class);
+        startActivity(intent);
+    }
+
 }
