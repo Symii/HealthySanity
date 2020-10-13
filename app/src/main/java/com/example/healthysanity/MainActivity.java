bdbb4b3addc13f128f59cd6f32e0ca0993cc2d1b
Symi <symi.contact@gmail.com>
2020-10-13 19:29:38 +0200

    Sidebar Menu

--- app/src/main/java/com/example/healthysanity/MainActivity.java
+++ app/src/main/java/com/example/healthysanity/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.healthysanity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final static String USERNAME = "Użytkownik";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView usernameTextView = (TextView) findViewById(R.id.textUsername);
+        usernameTextView.setText(USERNAME);
 
         LinearLayout categoryLayout = findViewById(R.id.layoutCategory);
         categoryLayout.setOnClickListener(new View.OnClickListener()
@@ -25,6 +31,9 @@ public class MainActivity extends AppCompatActivity
                 openCategoryActivity();
             }
         });
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
     }
 
